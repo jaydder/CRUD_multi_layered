6a11aa78816a4443915119166ff3b92ace7a7819
jaydder <jaydderfavoravel@gmail.com>
2025-10-08 04:20:20 -0300

    lint: fix imports in app file

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import os
-import uvicorn
+from os import environ
 from fastapi import FastAPI
+from uvicorn import run
 from utils.custom_logger import configure_logging
 from controller.user_controller import router as user_router
-
 
 configure_logging()
 
@@ -13,5 +12,5 @@ app.include_router(user_router)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    uvicorn.run('app:app', host='0.0.0.0', port=port, reload=True)
+    port = int(environ.get('PORT', 8000))
+    run('app:app', host='0.0.0.0', port=port, reload=True)
